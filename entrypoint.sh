6ef6724d7871d494baea1a09c4106733e21ff860
Raxul257 <luxar.pl@outlook.com>
2020-01-20 18:31:58 +0100

    rocket

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,11 @@ sleep 2
 
 cd /home/container
 
-./steam/steamcmd.sh +@sSteamCmdForcePlatformBitness 64 +login anonymous +force_install_dir /home/container +app_update 1110390 +quit
+./steam/steamcmd.sh +@sSteamCmdForcePlatformBitness 64 +login anonymous +force_install_dir /home/container/unturned +app_update 1110390 +quit
 
-ulimit -n 2048
-export LD_LIBRARY_PATH=$HOME/lib:$LD_LIBRARY_PATH
+curl -o Rocket.zip -L "https://github.com/RocketMod/Rocket.Unturned/releases/download/4.9.3.0/Rocket.Unturned.zip"
+unzip -o -q Rocket.zip
+cp /home/container/Modules /home/container/unturned/Modules
 
 MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
 echo ":/home/container$ ${MODIFIED_STARTUP}"
---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd unturned
+
+cp -f linux64/steamclient.so Unturned_Headless_Data/Plugins/x86_64/steamclient.so
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`dirname $0`/Unturned_Headless_Data/Plugins/x86_64/
+
+export TERM=xterm
+
+./Unturned_Headless.x86_64 -batchmode -nographics "unturned"
